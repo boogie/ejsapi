f18f5ca6f837243d918b10fcc65040eba874bf61
Andras Barthazi <andras@barthazi.hu>
2014-11-13 10:32:27 +0100

    add a button to email campaigns

--- example.js
+++ example.js
@@ -3,4 +3,11 @@ SuiteAPI.VCMS.on('sectionSelected', function(section) {
   console.log(section);
 });
 
+SuiteAPI.DOM.addButton({
+  'pageId': 'email-campaigns',
+  'block': 'create',
+  'title': 'Hello World',
+  'onclick': function() { alert('ALERT'); }
+});
+
 console.log(SuiteAPI.pageID);

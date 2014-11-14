47b19b195abb5aa626fabefd7b51edefb94d04b1
Andras Barthazi <andras@barthazi.hu>
2014-11-14 13:16:21 +0100

    VCMS panel

--- example.js
+++ example.js
@@ -4,36 +4,15 @@ SuiteAPI.onPage('vcms', function() {
     console.log(section);
   });
 
-  var customer_name = 'ff_addon_demo',
-    loading_image = 'https://clientservices.emarsys.net/' + customer_name +
-    '/images/ff_addon/loading_small.gif',
-    ok_image = 'https://clientservices.emarsys.net/' + customer_name +
-    '/images/ff_addon/haekchen.png',
-    cancel_image = 'https://clientservices.emarsys.net/' + customer_name +
-    '/images/ff_addon/delete_01.png';
+  var campaign = SuiteAPI.VCMS.getCampaign(),
+    campaign_id = campaign.id,
+    campaign_name = campaign.name,
+    customer_name = 'ff_addon_demo';
 
-  panelHTML = '';
-
-  panelHTML +=
-    '<div class="btn pull" id="btnUpdateDBSelector"><span>Datenbank aktualisieren</span></div>';
-  panelHTML += '<span id="search_loading_update_db_btn" > <img src="' +
-    loading_image + '"></span>';
-  panelHTML += '<span id="db_refresh_ok"> <img src="' + ok_image +
-    '"></span>';
-  panelHTML += '<br /><br />';
-
-  panelHTML += '<div id="divProduktschnittstelle">';
-  panelHTML +=
-    '<input type="text" id="txtProductSearch" class="key_event_ff_addon" size="30"/>';
-  panelHTML += '<div id="clear_result"><img src="' + cancel_image +
-    '"></div>';
-  panelHTML +=
-    '<div class="btn pull" id="btnProductSelector"><span>Produktsuche</span></div>';
-  panelHTML += '<span id="search_loading" > <img src="' + loading_image +
-    '"></span>';
-  panelHTML += '<br class="clearer" />';
-  panelHTML += '<div id="divProductSearchResults"></div>';
-  panelHTML += '<br class="clearer" />';
+  var panelHTML = '';
+  panelHTML += '<div id="productSearch">';
+  panelHTML += '<input type="text" id="txtProductSearch" size="30" />';
+  panelHTML += '<div class="btn pull" id="btnProductSelector"><span>Search</span></div>';
   panelHTML += '</div>';
 
   SuiteAPI.VCMS.setPanelContent('JS-API-Demo', panelHTML);
